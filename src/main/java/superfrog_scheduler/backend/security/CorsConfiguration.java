d0fcc0ee4e308542ece2338f3c0c4e138fbe4a78
Blake Good <blake.good@tcu.edu>
2024-04-15 19:49:57 -0500

    Added CORS configuration to test backend/frontend connection in web browser

---
+++ src/main/java/superfrog_scheduler/backend/security/CorsConfiguration.java
@@ -0,0 +1,20 @@
+package superfrog_scheduler.backend.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
+    }
+}

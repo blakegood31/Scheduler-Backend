f67fb89a719488d99b04493ad8af8c0ccd70d15e
Esau Rodriguez <esau.rodriguezolvera@tcu.edu>
2024-05-01 18:04:03 -0500

    def superFrogStudentSpecifications

--- src/main/java/superfrog_scheduler/backend/director/DirectorService.java
+++ src/main/java/superfrog_scheduler/backend/director/DirectorService.java
@@ -22,7 +22,6 @@ public class DirectorService {
     private final StudentRepository studentRepository;
 
    /* private final PasswordEncoder passwordEncoder;*/
-
     private final SuperFrogStudentSpecifications superFrogStudentSpecifications;
 
     public DirectorService(DirectorRepository directorRepository, StudentRepository studentRepository, SuperFrogStudentSpecifications superFrogStudentSpecifications) {
